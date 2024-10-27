f06e7480d0e8f68a17fb3809cebd104897998cb6
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2024-10-27 00:32:42 -0700

    confusion

--- main.gd
+++ main.gd
@@ -1,8 +1,7 @@
 extends Node
 
-
 const ID = "GardenGals.FishExtended" 
 onready var Lure = get_node("/root/SulayreLure")
-func _ready():#
+func _ready():
 	Lure.add_content(ID,"longhorn_cowfish","mod://Resources/longhorn_cowfish.tres")
 	Lure.add_content(ID,"sea_toad","mod://Resources/sea_toad.tres")
